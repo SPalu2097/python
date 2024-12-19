167fe42d8dfc2c3aa65e00ba8f8817a41ffe582e
SimonP <138118450+SPalu2097@users.noreply.github.com>
2024-12-19 17:42:56 +0200

    uued harjutused
    tehtud

---
+++ 06.py
@@ -0,0 +1,27 @@
+#ülesanne 6
+# 19.12.2024
+# Simon Palu
+# joonista skeem ja arvuta redeli pikkus
+# import math
+# import turtle
+# 
+# nurk = 53
+# korgus = 4.4
+# 
+# rad = math.radians(nurk)
+# 
+# kaugus = korgus/ math.tan(rad)
+# 
+# c = math.sqrt(math.pow(kaugus,2) + math.pow(korgus,2))
+# 
+# print(c)
+# kordaja = 100
+# 
+# turtle.forward(kaugus * kordaja)
+# turtle.left(90)
+# turtle.forward(korgus * kordaja)
+# turtle.left(90+53)
+# turtle.forward(c * kordaja)
+# 
+# turtle.done()
+
---
+++ 07.py
@@ -0,0 +1,13 @@
+# 19.12.2024
+# ülesanne 7
+# Simon Palu
+
+nimi = ["Jyri Pootsmann", "Mari Jyrgens", "Andsambel Maali", "Terminaator-Juuli kuus lumi on maas"]
+
+# for i in nimi:
+#     print(i)
+#for i in range(4):
+#    print(f"{i+1}. {nimi[i]}")
+    
+valik = int(input(" Vali lugu (1-4):"))
+print(f"Mängin: {nimi[valik-1]}")
