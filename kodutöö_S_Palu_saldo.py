ba9bdbf90e5e1d4fe6bf4f23bf0c98589d3f22fd
SimonP <138118450+SPalu2097@users.noreply.github.com>
2025-02-03 16:01:05 +0200

    tegin väikse paranduse!

--- kodutöö_S_Palu_saldo.py
+++ kodutöö_S_Palu_saldo.py
@@ -9,51 +9,60 @@
 #  toiming: String, mis tähistab tehtavat toimingut ('deposiit' või 'valjavote')
 #  summa: summa, mida soovitakse lisada või eemaldada
 
-algne_saldo = 100
+while True:
+    algne_saldo = 100
 
 
-def deposiit():
-     a = algne_saldo
-     b = summa
-     lopp_saldo= a+b
-     print(f"Teie kontojaak on nüüd: {lopp_saldo}")
+    def deposiit():
+        a = algne_saldo
+        b = summa
+        lopp_saldo= a+b
+        print(f"Teie kontojaak on nüüd: {lopp_saldo}")
 
-def valjavott():
-    if algne_saldo >= summa:
-        lopp_saldo = algne_saldo-summa
-        print(f"Teie kontojääk on {lopp_saldo} €")
-    elif algne_saldo < summa:
-        print("Sinu arvel pole piisavalt vahendeid!!!")
-    else:
-        print("Oled sisestanud vale väärtuse")
-
-
-
-print(f"Tere, Olen sinu pank ja sinu konto saldo on {algne_saldo}€ ")
-
-valik = str(input("Tee oma valik: 'deposiit', 'valjavott' või 'exit'  "))
-
-if valik == "deposiit":
-    print("Valisid raha sisestamise!")
-elif valik == "valjavott":
-    print("Valisid raha valjavotmise")
-elif valik == "exit":
-    print("Valisid lahkumise")
-    print("Head päeva Teile!")
-    breakpoint
-else:
-    print("Kirjutasid midagi valesti")
-
-summa = float(input("Kui palju raha: "))
-
-if valik == "deposiit":
-    deposiit()
-elif valik == "valjavott":
-    valjavott()
-else:
-    print("Sisestage palun õige väärtus valikust!")
+    def valjavott():
+        if algne_saldo >= summa:
+            lopp_saldo = algne_saldo-summa
+            print(f"Teie kontojääk on {lopp_saldo} €")
+        elif algne_saldo < summa:
+            print("Sinu arvel pole piisavalt vahendeid!!!")
+        else:
+            print("Oled sisestanud vale väärtuse")
 
 
 
+    print(f"Tere, Olen sinu pank ja sinu konto saldo on {algne_saldo}€ ")
+
+    try:
+        valik = str(input("Tee oma valik: 'deposiit', 'valjavott' või 'exit'  "))
 
 
+        if valik == "deposiit":
+            print("Valisid raha sisestamise!")
+        elif valik == "valjavott":
+            print("Valisid raha valjavotmise")
+        elif valik == "exit":
+            print("Valisid lahkumise")
+            print("Head päeva Teile!")
+            breakpoint
+        else:
+            print("Kirjutasid midagi valesti")
+            breakpoint
+    except:
+        print("Sisesta õigesti oma valik!")
+
+    try:
+
+        summa = float(input("Kui palju raha: "))
+
+        if valik == "deposiit":
+            deposiit()
+        elif valik == "valjavott":
+            valjavott()
+        else:
+            print("Sisestage palun õige väärtus valikust!")
+    except:
+        print("Palun sisesta summa numbrites!")
+
+    tegevus = int(input("Kas sulgen programmi? jah on 1 ja ei on 0: "))
+    if tegevus != 1:
+        break
