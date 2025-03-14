cb5fbd3dc12d66dc9b0a17f4c9be8f5ecfa2cccb
SimonP <138118450+SPalu2097@users.noreply.github.com>
2025-03-14 10:16:40 +0200

    tehtud

--- raamatud_S_Palu.py
+++ raamatud_S_Palu.py
@@ -14,13 +14,26 @@ if response.status_code == 200:
     raamatud = data.get("raamatud", [])
     #print(raamatud)
     no_saadavus= []
-    mitu_raam_2010 = 0
+    mitu_raamat = []
+    vanim_raamat = raamatud[0]
+    #print(vanim_raamat['väljaandmise_aasta'])
    
+    # for raamat in raamatud:
+    #     if raamat['saadavus'] == False:
+    #         no_saadavus.append(raamat)
+    # for i in no_saadavus:
+    #     print(i['pealkiri'])
+    
+    # for r in raamatud:
+    #     if r['väljaandmise_aasta'] >= 2010:
+    #         mitu_raamat.append(r)
+    # print(f"Raamatuid, mis on välja antud alates 2010. aastast: {len(mitu_raamat)}")
+            
     for raamat in raamatud:
-        if raamat['saadavus'] == False:
-            no_saadavus.append(raamat)
-    for i in no_saadavus:
-        print(i['pealkiri'])
+        if raamat['väljaandmise_aasta'] < vanim_raamat['väljaandmise_aasta']:
+            vanim_raamat=raamat
+    print(vanim_raamat)
+    #print(f"Vanim raamat on: {min(vanim_raamat)} ")
     
 
 
